2cd55a70e1066c620bad4e99bf17a5ca7baf6058
Olivier Baumann <olivier@ovbm.co>
2017-04-26 10:50:40 +0200

    initial 1.0 release

---
+++ models/Band.js
@@ -0,0 +1,38 @@
+var keystone = require('keystone');
+var Types = keystone.Field.Types;
+
+/**
+ * User Model
+ * ==========
+ */
+var Band = new keystone.List('Band', {
+	autokey: { path: 'slug', from: 'title', unique: true },
+	map: { name: 'title' },
+	defaultSort: '-title',
+});
+
+Band.add({
+	title: { type: String, required: true },
+	state: { type: Types.Select, options: 'draft, published, archived', default: 'draft' },
+	author: { type: Types.Relationship, ref: 'User' },
+	createdAt: { type: Date, default: Date.now },
+	publishedAt: Date,
+	image: {
+		type: Types.CloudinaryImage,
+		publicID: 'slug',
+		autoCleanup: true,
+	},
+	content: {
+		description: { type: Types.Text },
+		lineup: { type: Types.Text },
+		linktext: { type: Types.Text },
+		link: { type: Types.Url },
+		facebooklink: { type: Types.Url },
+	},
+});
+
+/**
+ * Registration
+ */
+Band.defaultColumns = 'title, createdAt, publishedAt';
+Band.register();
---
+++ routes/views/index.js
@@ -0,0 +1,29 @@
+var keystone = require('keystone');
+var Band = keystone.list('Band');
+
+
+exports = module.exports = function (req, res) {
+
+	var view = new keystone.View(req, res);
+	var locals = res.locals;
+
+	// locals.section is used to set the currently selected
+	// item in the header navigation.
+	locals.section = 'home';
+	locals.bands = [];
+
+	// Load all bands
+	view.on('init', function (next) {
+		Band.model.find()
+			.where('state', 'published')
+			.sort('publishedAt')
+			.exec(function (err, bands) {
+				if (err) res.err(err);
+				locals.bands = bands;
+				next();
+			});
+	});
+
+	// Render the view
+	view.render('index');
+};
